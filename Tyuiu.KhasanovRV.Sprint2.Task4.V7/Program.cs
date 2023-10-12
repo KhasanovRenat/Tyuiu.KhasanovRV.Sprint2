8d3e16387d5b863cf8ddfcf390719b15d14ab474
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-10-12 18:00:45 +0500

    Task5

---
+++ Tyuiu.KhasanovRV.Sprint2.Task4.V7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KhasanovRV.Sprint2.Task5.V7.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint2.Task5.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Хасанов Р. В. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Хасанов Ренат Владиславович | СМАРТб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат. Условие: С начала 1990 года  *");
+            Console.WriteLine("* по некоторый день прошло n месяцев и 2 дня. Определить название месяца  *");
+            Console.WriteLine("* (январь, февраль и т. п.) этого дня.                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int y = 1990, n;
+
+            Console.WriteLine("Введите кол-во прошедших месяцев:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var s = ds.FindMonthName(y, n);
+            Console.WriteLine("Сейчас на дворе " + s);
+            Console.ReadKey();
+        }
+    }
+}
